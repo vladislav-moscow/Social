3748c59baa3aa813a13284ae1f859dc97075028b
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-11 18:18:19 +0300

    компонет посты и стили к нему

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { useUserStore } from '../../store/useUserStore';
+import { MoreVert } from '@mui/icons-material';
+//import { Users } from '../../Data';
+
+import './post.css';
+
+const Post = ({ post }) => {
+	// Стейт для хранения информации о лайках
+	const [likeState, setLikeState] = useState({
+		likeCount: post.like,
+		isLiked: false,
+	});
+
+	/// Функция добавления и удаления лайка с поста
+	const likeHandler = () => {
+		setLikeState((prevState) => ({
+			likeCount: prevState.isLiked ? prevState.likeCount - 1 : prevState.likeCount + 1,
+			isLiked: !prevState.isLiked,
+		}));
+	};
+
+	// Получаем функцию getUserById из Zustand
+	const getUserById = useUserStore((state) => state.getUserById);
+
+	// Ищем пользователя по id с использованием Zustand
+	const user = getUserById(post?.userId);
+
+	return (
+		<div className='post'>
+			<div className='postWrapper'>
+				<div className='postTop'>
+					<div className='postTopLeft'>
+					<img
+							className='postProfileImg'
+							src={user?.profilePicture || 'default-avatar.png'}
+							alt={user ? `${user.username}'s avatar` : 'default avatar'}
+						/>
+						<span className='postUsername'>{user?.username || 'Unknown User'}</span>
+						<span className='postDate'>{post.date}</span>
+					</div>
+					<div className='postTopRight'>
+						<MoreVert />
+					</div>
+				</div>
+				<div className='postCenter'>
+					<span className='postText'>{post?.desc}</span>
+					<img className='postImg' src={post?.photo} alt='ImgPost' />
+				</div>
+				<div className='postBottom'>
+					<div className='postBottomLeft'>
+						<img
+							className='likeIcon'
+							src='assets/heart.png'
+							onClick={likeHandler}
+							alt=''
+						/>
+						<span className='postLikeCounter'>{likeState.likeCount} человек понравилось</span>
+					</div>
+					<div className='postBottomRight'>
+						<span className='postCommentText'>{post.comment} комментария</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Post;
