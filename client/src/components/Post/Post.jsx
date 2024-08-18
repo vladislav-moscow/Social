9e7b8b1f3ccc7edc24657755ea9e05d73399dff4
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-18 20:58:35 +0300

    в компоненте добавил что бы дата была коректой добавил стор и получаю данный из бэка

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react';
-import { useUserStore } from '../../store/useUserStore';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+//import { useUserStore } from '../../store/useUserStore';
 import { MoreVert } from '@mui/icons-material';
 import './post.css';
+import axios from 'axios';
+import { format, register } from 'timeago.js';
+import ru from 'timeago.js/lib/lang/ru';
+
+// Регистрируем русскую локализацию
+register('ru', ru);
 
 const Post = ({ post }) => {
-	// Стейт для хранения информации о лайках
+	// Форматируем дату с помощью timeago.js
+  const formattedDate = format(post.createdAt, 'ru');
 	const [likeState, setLikeState] = useState({
-		likeCount: post.like,
+		likeCount: post.likes.length,
 		isLiked: false,
 	});
+
+	const [user, setUser] = useState({});
+
 	const PF = import.meta.env.VITE_PUBLIC_FOLDER;
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			const res = await axios.get(`/api/users?userId=${post.userId}`);
+			setUser(res.data);
+		};
+		fetchUser();
+	}, [post.userId]);
 
 	/// Функция добавления и удаления лайка с поста
 	const likeHandler = () => {
@@ -22,25 +42,28 @@ const Post = ({ post }) => {
 	};
 
 	// Получаем функцию getUserById из Zustand
-	const getUserById = useUserStore((state) => state.getUserById);
+	//const getUserById = useUserStore((state) => state.getUserById);
 
 	// Ищем пользователя по id с использованием Zustand
-	const user = getUserById(post?.userId);
+	//const user = getUserById(post?.userId);
 
 	return (
 		<div className='post'>
 			<div className='postWrapper'>
 				<div className='postTop'>
 					<div className='postTopLeft'>
+						<Link to={`/profile/${user.username}`}>
 						<img
 							className='postProfileImg'
-							src={PF+user?.profilePicture || 'default-avatar.png'}
+							src={PF + user?.profilePicture || 'default-avatar.png'}
 							alt={user ? `${user.username}'s avatar` : 'default avatar'}
 						/>
+						</Link>
+						
 						<span className='postUsername'>
 							{user?.username || 'Unknown User'}
 						</span>
-						<span className='postDate'>{post.date}</span>
+						<span className='postDate'>{formattedDate}</span>
 					</div>
 					<div className='postTopRight'>
 						<MoreVert />
@@ -48,7 +71,7 @@ const Post = ({ post }) => {
 				</div>
 				<div className='postCenter'>
 					<span className='postText'>{post?.desc}</span>
-					<img className='postImg' src={PF+post?.photo} alt='ImgPost' />
+					<img className='postImg' src={PF + post?.img} alt='ImgPost' />
 				</div>
 				<div className='postBottom'>
 					<div className='postBottomLeft'>
