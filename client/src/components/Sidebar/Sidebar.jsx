bfeecaecd23ba4e5ec102dc230b07b6e4c92c3a7
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-18 21:03:11 +0300

    подключил топбару и сайдбару линки и

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,42 @@
-import { Bookmark, Chat, Event, Group, HelpOutline, PlayCircleFilledOutlined, RssFeed, School, WorkOutline } from '@mui/icons-material';
+import {
+	Chat,
+	Group,
+	PlayCircleFilledOutlined,
+	RssFeed,
+	School,
+	WorkOutline,
+} from '@mui/icons-material';
 import { useUserStore } from '../../store/useUserStore';
 import Friend from '../Friend/Friend';
+import { Link } from 'react-router-dom';
 
 import './sidebar.css';
 
 const Sidebar = () => {
 	// Получаем список пользователей из Zustand Store
-  const users = useUserStore((state) => state.users);
-	
+	const users = useUserStore((state) => state.users);
+
 	return (
 		<div className='sidebar'>
 			<div className='sidebarWrapper'>
 				<ul className='sidebarList'>
 					<li className='sidebarListItem'>
-						<RssFeed htmlColor='#1877f2' className='sidebarIcon' />
-						<span className='sidebarListItemText'>Новости</span>
+						<Link to={'/'}>
+							<RssFeed htmlColor='#1877f2' className='sidebarIcon' />
+							<span className='sidebarListItemText'>Новости</span>
+						</Link>
 					</li>
 					<li className='sidebarListItem'>
-						<Chat htmlColor='#1877f2' className='sidebarIcon' />
-						<span className='sidebarListItemText'>Чат</span>
+						<Link to={'/chat'}>
+							<Chat htmlColor='#1877f2' className='sidebarIcon' />
+							<span className='sidebarListItemText'>Чат</span>
+						</Link>
 					</li>
 					<li className='sidebarListItem'>
-						<PlayCircleFilledOutlined htmlColor='#1877f2' className='sidebarIcon' />
+						<PlayCircleFilledOutlined
+							htmlColor='#1877f2'
+							className='sidebarIcon'
+						/>
 						<span className='sidebarListItemText'>Видео</span>
 					</li>
 					<li className='sidebarListItem'>
@@ -29,20 +44,8 @@ const Sidebar = () => {
 						<span className='sidebarListItemText'>Группы</span>
 					</li>
 					<li className='sidebarListItem'>
-						<Bookmark htmlColor='#1877f2' className='sidebarIcon' />
-						<span className='sidebarListItemText'>Закладки</span>
-					</li>
-					<li className='sidebarListItem'>
-						<HelpOutline htmlColor='#1877f2' className='sidebarIcon' />
-						<span className='sidebarListItemText'>Вопросы</span>
-					</li>
-					<li className='sidebarListItem'>
 						<WorkOutline htmlColor='#1877f2' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Работа</span>
-					</li>
-					<li className='sidebarListItem'>
-						<Event htmlColor='#1877f2' className='sidebarIcon' />
-						<span className='sidebarListItemText'>События</span>
 					</li>
 					<li className='sidebarListItem'>
 						<School htmlColor='#1877f2' className='sidebarIcon' />
@@ -50,10 +53,11 @@ const Sidebar = () => {
 					</li>
 				</ul>
 				<hr className='sidebarHr' />
+				<h2 className='sidebarMyFriend'>Мои друзья:</h2>
 				<ul className='sidebarFriendList'>
-				{users.map((user) => (
-            <Friend key={user.id} user={user} />
-          ))}
+					{users.map((user) => (
+						<Friend key={user.id} user={user} />
+					))}
 				</ul>
 			</div>
 		</div>
--- client/src/components/Topbar/Topbar.jsx
+++ client/src/components/Topbar/Topbar.jsx
@@ -1,43 +1,50 @@
 import { Chat, Notifications, Person, Search } from '@mui/icons-material';
 import './topbar.css';
+import { Link } from 'react-router-dom';
 
 const Topbar = () => {
 	return (
-		<div className="topbarContainer">
-      <div className="topbarLeft">
-        <span className="logo">SocialApp</span>
-      </div>
-      <div className="topbarCenter">
-        <div className="searchbar">
-          <Search className="searchIcon" />
-          <input
-            placeholder="Поиск друзей, постов или видео"
-            className="searchInput"
-          />
-        </div>
-      </div>
-      <div className="topbarRight">
-        <div className="topbarLinks">
-          <span className="topbarLink">Homepage</span>
-          <span className="topbarLink">Timeline</span>
-        </div>
-        <div className="topbarIcons">
-          <div className="topbarIconItem">
-            <Person />
-            <span className="topbarIconBadge">1</span>
-          </div>
-          <div className="topbarIconItem">
-            <Chat />
-            <span className="topbarIconBadge">2</span>
-          </div>
-          <div className="topbarIconItem">
-            <Notifications />
-            <span className="topbarIconBadge">1</span>
-          </div>
-        </div>
-        <img src="/assets/person/1.jpg" alt="avatarPerson" className="topbarImg"/>
-      </div>
-    </div>
+		<div className='topbarContainer'>
+			<div className='topbarLeft'>
+				<Link to={'/'} className='logo'>
+					<span className='logo'>SocialApp</span>
+				</Link>
+			</div>
+			<div className='topbarCenter'>
+				<div className='searchbar'>
+					<Search className='searchIcon' />
+					<input
+						placeholder='Поиск друзей, постов или видео'
+						className='searchInput'
+					/>
+				</div>
+			</div>
+			<div className='topbarRight'>
+				<div className='topbarLinks'>
+					{/*<span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>*/}
+				</div>
+				<div className='topbarIcons'>
+					<div className='topbarIconItem'>
+						<Person />
+						<span className='topbarIconBadge'>1</span>
+					</div>
+					<div className='topbarIconItem'>
+						<Chat />
+						<span className='topbarIconBadge'>2</span>
+					</div>
+					<div className='topbarIconItem'>
+						<Notifications />
+						<span className='topbarIconBadge'>1</span>
+					</div>
+				</div>
+				<img
+					src='/assets/person/1.jpg'
+					alt='avatarPerson'
+					className='topbarImg'
+				/>
+			</div>
+		</div>
 	);
 };
 
