789f0712aa292543a61421322073fe3c2ee60665
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-18 20:56:47 +0300

    обновил роутер что бы если нет пользователя страница регистрации

--- client/src/components/AppRoutes/AppRoutes.jsx
+++ client/src/components/AppRoutes/AppRoutes.jsx
@@ -1,19 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../../pages/Login/Login';
 import Register from '../../pages/Register/Register';
 import Home from '../../pages/Home/Home';
 import Profile from '../../pages/Profile/Profile';
 import Chat from '../../pages/Chat/Chat';
+import useAuthStore from '../../store/useAuthStore'; // Путь к вашему Zustand store
 
 /** Корневой компонент приложения с роутами */
-const AppRoutes = () => (
-	<Routes>
-		<Route path='/' element={<Home />} />
-		<Route path='/login' element={<Login />} />
-		<Route path='/register' element={<Register />} />
-		<Route path='/profile/:username' element={<Profile />} />
-		<Route path='/chat' element={<Chat />} />
-	</Routes>
-);
+const AppRoutes = () => {
+	const { user } = useAuthStore(); // Получаем пользователя из Zustand store
+
+	return (
+		<Routes>
+			<Route path='/' element={user ? <Home /> : <Register />} />
+			<Route
+				path='/login'
+				element={user ? <Navigate to='/' replace /> : <Login />}
+			/>
+			<Route
+				path='/register'
+				element={user ? <Navigate to='/' replace /> : <Register />}
+			/>
+			<Route path='/profile/:username' element={<Profile />} />
+			<Route path='/chat' element={<Chat />} />
+		</Routes>
+	);
+};
 
 export default AppRoutes;
