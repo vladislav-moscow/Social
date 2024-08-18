f12e71c7534c1495bd9317fd34ff624992e3d6ca
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-18 21:05:11 +0300

    сделал форму входа и валидацию формы

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,6 +1,27 @@
+import { useForm } from '../../hooks/useForm';
 import './login.css';
+import useAuthStore from '../../store/useAuthStore';
+import { Link } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
+import { validateForm } from '../../utils/validators';
 
 const Login = () => {
+	const { formValues, formErrors, handleInput, resetForm } = useForm({
+		email: '',
+		password: '',
+	});
+	// Извлечение состояния и действий из Zustand store
+	const { isFetching, loginCall } = useAuthStore();
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+
+		const validationErrors = validateForm(formValues);
+		if (Object.keys(validationErrors).length === 0) {
+			await loginCall(formValues);
+		}
+	};
+
 	return (
 		<div className='login'>
 			<div className='loginWrapper'>
@@ -11,17 +32,43 @@ const Login = () => {
 					</span>
 				</div>
 				<div className='loginRight'>
-					<div className='loginBox'>
-						<input placeholder='Ваша почта' className='loginInput' />
-						<input placeholder='Пароль' className='loginInput' />
-						<button className='loginButton'>Вход</button>
+					<form className='loginBox' onSubmit={handleClick}>
+						<input
+							type='email'
+							name='email'
+							placeholder='Ваша почта'
+							className='loginInput'
+							value={formValues.email}
+							onChange={handleInput}
+						/>
+						{formErrors.email && (
+							<span className='error'>{formErrors.email}</span>
+						)}
+						<input
+							type='password'
+							name='password'
+							placeholder='Пароль'
+							className='loginInput'
+							value={formValues.password}
+							onChange={handleInput}
+						/>
+						{formErrors.password && (
+							<span className='error'>{formErrors.password}</span>
+						)}
+						<button className='loginButton' type='submit' disabled={isFetching}>
+							{isFetching ? (
+								<CircularProgress color='success' size={30} />
+							) : (
+								'Войти'
+							)}
+						</button>
 						<div className='loginWrapperBtn'>
 							<span className='loginForgot'>Забыли пароль?</span>
-							<span className='loginRegisterButton'>
+							<Link to={'/register'} className='loginRegisterButton'>
 								Вы еще не зарегистрированы?
-							</span>
+							</Link>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
