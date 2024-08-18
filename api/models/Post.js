026070b3f18eda47695d10e7d29d827ff6dcd62f
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-18 20:55:52 +0300

    обновил бэk на ECM

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,25 +1,25 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 //создаем новую таблицу постов
 const PostSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: String,
-      required: true,
-    },
-    desc: {
-      type: String,
-      max: 500,
-    },
-    img: {
-      type: String,
-    },
-    likes: {
-      type: Array,
-      default: [],
-    },
-  },
-  { timestamps: true }
+	{
+		userId: {
+			type: String,
+			required: true,
+		},
+		desc: {
+			type: String,
+			max: 500,
+		},
+		img: {
+			type: String,
+		},
+		likes: {
+			type: Array,
+			default: [],
+		},
+	},
+	{ timestamps: true }
 );
 
-module.exports = mongoose.model("Post", PostSchema);
+export default mongoose.model('Post', PostSchema);
