f0421361e0e548ecbe6efebaebee15adc32ceaa6
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-21 21:59:29 +0300

    подключил зависимости и настроил загрузку файлов на стороне бэка

--- api/index.js
+++ api/index.js
@@ -4,13 +4,21 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import multer from 'multer';
+import path from 'path';
 import userRoute from './routes/users.js';
 import authRoute from './routes/auth.js';
 import postsRoute from './routes/posts.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
+
+// Получаем путь к текущему файлу
+const __filename = fileURLToPath(import.meta.url);
+// Получаем путь к директории
+const __dirname = path.dirname(__filename);
 
 //подключаемся к базе данных
 const connect = async () => {
@@ -22,10 +30,30 @@ const connect = async () => {
 	}
 };
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // Роуты
 app.use('/api/users', userRoute);
@@ -33,6 +61,6 @@ app.use('/api/auth', authRoute);
 app.use('/api/posts', postsRoute);
 
 app.listen(6600, () => {
-  connect();
-  console.log('север ОК');
+	connect();
+	console.log('север ОК');
 });
