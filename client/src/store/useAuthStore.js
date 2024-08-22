a838615372e1625e546351cbe0e24fafbd3e7b59
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-22 22:24:46 +0300

    разделил стор по логике посты пользователи и авторизация

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -9,9 +9,10 @@ import axios from 'axios';
  */
 const useAuthStore = create(
 	devtools((set, get) => ({
-		user: JSON.parse(localStorage.getItem('user')) || null,
-		isFetching: false,
-		error: false,
+		// Начальное состояние стора
+		user: JSON.parse(localStorage.getItem('user')) || null, // Инициализируем состояние пользователя из localStorage или null, если пользователь не аутентифицирован.
+		isFetching: false, // Флаг загрузки данных, чтобы отслеживать состояние загрузки.
+		error: false, // Флаг ошибки, чтобы отслеживать состояние ошибок.
 
 		// ===== Селекторы =====
 
@@ -19,42 +20,44 @@ const useAuthStore = create(
 		 * Получает текущего пользователя из состояния.
 		 * @returns {Object|null} - Данные пользователя или null, если пользователь не аутентифицирован.
 		 */
-		getUser: () => get().user,
+		getUser: () => get().user, // Возвращает текущего пользователя из состояния.
 
 		/**
 		 * Проверяет, аутентифицирован ли пользователь.
 		 * @returns {boolean} - true, если пользователь аутентифицирован, иначе false.
 		 */
-		isAuthenticated: () => !!get().user,
+		isAuthenticated: () => !!get().user, // Проверяет, аутентифицирован ли пользователь, и возвращает boolean.
 
 		/**
 		 * Получает конкретное свойство пользователя.
 		 * @param {string} key - Ключ свойства, которое необходимо получить.
 		 * @returns {*} - Значение свойства или undefined, если пользователь не аутентифицирован или свойство не существует.
 		 */
-		getUserProperty: (key) => get().user?.[key],
+		getUserProperty: (key) => get().user?.[key], // Получает значение конкретного свойства пользователя.
+
+		// ===== Действия =====
 
 		/**
 		 * Начало процесса аутентификации (вход в систему).
 		 * Устанавливает состояние загрузки и сбрасывает ошибки.
 		 */
-		loginStart: () => set({ isFetching: true, error: false }),
+		loginStart: () => set({ isFetching: true, error: false }), // Устанавливает флаги isFetching и error перед началом процесса аутентификации.
+
 		/**
 		 * Успешный вход в систему.
 		 * @param {Object} user - Данные пользователя, полученные с сервера.
 		 * Сохраняет пользователя в состоянии и localStorage.
 		 */
-
 		loginSuccess: (user) => {
-			localStorage.setItem('user', JSON.stringify(user));
-			set({ user, isFetching: false, error: false });
+			localStorage.setItem('user', JSON.stringify(user)); // Сохраняем данные пользователя в localStorage.
+			set({ user, isFetching: false, error: false }); // Обновляем состояние с данными пользователя и сбрасываем флаги загрузки и ошибок.
 		},
 
 		/**
 		 * Неудачный вход в систему.
 		 * Сбрасывает состояние загрузки и устанавливает ошибку.
 		 */
-		loginFailure: () => set({ isFetching: false, error: true }),
+		loginFailure: () => set({ isFetching: false, error: true }), // Сбрасываем флаг загрузки и устанавливаем флаг ошибки в случае неудачного входа.
 
 		/**
 		 * Асинхронная функция для выполнения входа в систему.
@@ -62,16 +65,18 @@ const useAuthStore = create(
 		 * Выполняет запрос на сервер и сохраняет данные пользователя при успешной аутентификации.
 		 */
 		loginCall: async (userCredential) => {
-			set({ isFetching: true, error: false });
+			set({ isFetching: true, error: false }); // Устанавливаем состояние загрузки перед запросом.
 			try {
+				// Выполняем запрос на сервер для аутентификации пользователя.
 				const res = await axios.post('/api/auth/login', userCredential);
+				// Успешно аутентифицированного пользователя сохраняем в состояние и localStorage.
 				set({ user: res.data, isFetching: false, error: false });
 				localStorage.setItem('user', JSON.stringify(res.data));
 			} catch (err) {
-				// Здесь обрабатываем сообщение об ошибке, если оно есть
+				// В случае ошибки устанавливаем флаг ошибки и сохраняем сообщение об ошибке.
 				set({
 					isFetching: false,
-					error: err.response?.data?.message || 'Неверный пароль',
+					error: err.response?.data?.message || 'Неверный пароль', // Используем сообщение от сервера, если оно есть, иначе - стандартное сообщение.
 				});
 			}
 		},
@@ -82,18 +87,21 @@ const useAuthStore = create(
 		 * Выполняет запрос на регистрацию пользователя.
 		 */
 		registerCall: async (userData) => {
-			set({ isFetching: true, error: false });
+			set({ isFetching: true, error: false }); // Устанавливаем состояние загрузки перед регистрацией.
 			try {
+				// Выполняем запрос на сервер для регистрации нового пользователя.
 				await axios.post('/api/auth/register', userData);
-				set({ isFetching: false, error: false });
-				// Вы можете добавить автоматический вход после регистрации, если это необходимо:
+				set({ isFetching: false, error: false }); // Сбрасываем флаги после успешной регистрации.
+
+				// Автоматический вход после регистрации.
 				const res = await axios.post('/api/auth/login', {
 					email: userData.email,
 					password: userData.password,
 				});
-				set({ user: res.data, isFetching: false, error: false });
+				set({ user: res.data, isFetching: false, error: false }); // Сохраняем данные пользователя в состоянии и localStorage.
 				localStorage.setItem('user', JSON.stringify(res.data));
 			} catch (err) {
+				// В случае ошибки сохраняем состояние ошибки.
 				set({ isFetching: false, error: true });
 			}
 		},
@@ -102,15 +110,15 @@ const useAuthStore = create(
 		 * Очистка ошибок.
 		 * Сбрасывает состояние ошибок.
 		 */
-		clearError: () => set({ error: false }),
+		clearError: () => set({ error: false }), // Сбрасываем флаг ошибки.
 
 		/**
 		 * Действие для выхода из системы.
 		 * Очищает данные пользователя из состояния и localStorage.
 		 */
 		logout: () => {
-			localStorage.removeItem('user');
-			set({ user: null, isFetching: false, error: false });
+			localStorage.removeItem('user'); // Удаляем данные пользователя из localStorage.
+			set({ user: null, isFetching: false, error: false }); // Сбрасываем состояние пользователя и флаги.
 		},
 
 		/**
@@ -119,6 +127,7 @@ const useAuthStore = create(
 		 */
 		follow: (userId) =>
 			set((state) => ({
+				// Добавляем userId к списку подписок текущего пользователя.
 				user: {
 					...state.user,
 					followings: [...state.user.followings, userId],
@@ -131,6 +140,7 @@ const useAuthStore = create(
 		 */
 		unfollow: (userId) =>
 			set((state) => ({
+				// Удаляем userId из списка подписок текущего пользователя.
 				user: {
 					...state.user,
 					followings: state.user.followings.filter(
--- client/src/store/usePostStore.js
+++ client/src/store/usePostStore.js
@@ -1,26 +1,29 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import axios from 'axios';
+import useUserStore from './useUserStore'; // Импортируем новый стор
 
 /**
  * Zustand store для управления состоянием постов.
- * Включает действия для получения постов и их сохранения.
+ * Включает действия для получения постов, их сортировки, сохранения, лайков и очистки состояния.
  */
 const usePostStore = create(
 	devtools((set, get) => ({
-		posts: JSON.parse(localStorage.getItem('posts')) || [],
-		postUsers: {}, // Хранение пользователей по ID
-		isFetching: false,
-		error: false,
+		// Начальное состояние стора
+		posts: JSON.parse(localStorage.getItem('posts')) || [], // Список постов, изначально загруженный из localStorage (если есть).
+		isFetching: false, // Флаг загрузки данных, чтобы отслеживать состояние загрузки.
+		error: false, // Флаг ошибки, чтобы отслеживать состояние ошибок.
 
 		/**
 		 * Устанавливает состояние загрузки и сбрасывает ошибки.
+		 * Используется перед началом асинхронных операций, таких как загрузка постов.
 		 */
 		fetchPostsStart: () => set({ isFetching: true, error: false }),
 
 		/**
 		 * Успешное получение постов.
 		 * @param {Array} posts - Массив постов, полученных с сервера.
+		 * Сортирует посты по дате создания, сохраняет их в localStorage и обновляет состояние стора.
 		 */
 		fetchPostsSuccess: (posts) => {
 			// Сортируем посты по дате создания (от новых к старым)
@@ -29,13 +32,14 @@ const usePostStore = create(
 			);
 
 			// Сохраняем отсортированные посты в localStorage и Zustand store
-			localStorage.setItem('posts', JSON.stringify(sortedPosts));
-			set({ posts: sortedPosts, isFetching: false, error: false });
+			localStorage.setItem('posts', JSON.stringify(sortedPosts)); // Сохранение постов в localStorage для возможности восстановления состояния при перезагрузке.
+			set({ posts: sortedPosts, isFetching: false, error: false }); // Обновление состояния стора.
 		},
 
 		/**
-		 * Ошибка при получении постов.
+		 * Обработка ошибки при получении постов.
 		 * @param {string} errorMessage - Сообщение об ошибке.
+		 * Сбрасывает состояние загрузки и сохраняет сообщение об ошибке.
 		 */
 		fetchPostsFailure: (errorMessage) =>
 			set({ isFetching: false, error: errorMessage }),
@@ -43,68 +47,60 @@ const usePostStore = create(
 		/**
 		 * Асинхронная функция для получения постов.
 		 * @param {string} userId - ID пользователя, для которого загружаются посты.
+		 * @param {string} username - Имя пользователя (опционально), для которого загружаются посты.
 		 */
 		fetchPosts: async (userId, username) => {
-			set({ isFetching: true, error: false });
+			set({ isFetching: true, error: false }); // Устанавливаем состояние загрузки перед запросом.
 			try {
+				// В зависимости от наличия username выполняем соответствующий запрос к API.
 				const res = username
-					? await axios.get(`/api/posts/profile/${username}`)
-					: await axios.get(`/api/posts/timeline/${userId}`);
+					? await axios.get(`/api/posts/profile/${username}`) // Получение постов по имени пользователя.
+					: await axios.get(`/api/posts/timeline/${userId}`); // Получение постов по ID пользователя (таймлайн).
 
 				// Сортируем полученные посты по дате и сохраняем
 				const sortedPosts = res.data.sort(
 					(a, b) => new Date(b.createdAt) - new Date(a.createdAt)
 				);
-				set({ posts: sortedPosts, isFetching: false, error: false });
-				localStorage.setItem('posts', JSON.stringify(sortedPosts));
+				set({ posts: sortedPosts, isFetching: false, error: false }); // Обновляем состояние с полученными постами.
+				localStorage.setItem('posts', JSON.stringify(sortedPosts)); // Сохраняем посты в localStorage.
 			} catch (err) {
+				// В случае ошибки, сохраняем сообщение об ошибке.
 				set({
 					isFetching: false,
-					error: err.response?.data?.message || 'Ошибка загрузки постов',
+					error: err.response?.data?.message || 'Ошибка загрузки постов', // Обработка ошибок от сервера или использование стандартного сообщения.
 				});
 			}
 		},
 
 		/**
 		 * Очистка состояния постов.
+		 * Удаляет посты из localStorage и сбрасывает состояние стора.
 		 */
 		clearPosts: () => {
-			localStorage.removeItem('posts');
-			set({ posts: [], isFetching: false, error: false });
+			localStorage.removeItem('posts'); // Удаление постов из localStorage.
+			set({ posts: [], isFetching: false, error: false }); // Сброс состояния постов.
 		},
 
 		/**
-     * Асинхронная функция для получения данных пользователя, создавшего пост.
-     * @param {string} userId - ID пользователя, создавшего пост.
-     */
-    fetchPostUser: async (userId) => {
-      const existingUser = get().postUsers[userId];
-
-      // Если пользователь уже загружен, ничего не делаем
-      if (existingUser) {
-        return;
-      }
-
-      try {
-        const res = await axios.get(`/api/users?userId=${userId}`);
-        set((state) => ({
-          postUsers: { ...state.postUsers, [userId]: res.data },
-        }));
-      } catch (err) {
-        console.error('Ошибка загрузки пользователя поста:', err);
-      }
-    },
+		 * Асинхронная функция для получения данных пользователя, создавшего пост.
+		 * @param {string} userId - ID пользователя, создавшего пост.
+		 */
+		fetchPostUser: (userId) => {
+			const fetchUser = useUserStore.getState().fetchUser; // Получаем функцию fetchUser из useUserStore для получения данных пользователя.
+			fetchUser(userId); // Выполняем вызов функции для загрузки данных пользователя по его ID.
+		},
 
 		/**
 		 * Асинхронная функция для лайка поста.
 		 * @param {string} postId - ID поста, который лайкаем.
 		 * @param {string} userId - ID пользователя, который лайкает пост.
+		 * Выполняет PUT-запрос к серверу для обновления информации о лайках.
 		 */
 		likePost: async (postId, userId) => {
 			try {
-				await axios.put(`/api/posts/${postId}/like`, { userId });
+				await axios.put(`/api/posts/${postId}/like`, { userId }); // Выполнение запроса для обновления лайков на сервере.
 			} catch (err) {
-				console.error('Ошибка лайка поста:', err);
+				console.error('Ошибка лайка поста:', err); // Вывод ошибки в консоль в случае неудачи.
 			}
 		},
 
@@ -112,29 +108,32 @@ const usePostStore = create(
 		 * Метод для переключения лайка на посте.
 		 * @param {string} postId - ID поста, который лайкаем.
 		 * @param {string} userId - ID пользователя, который лайкает пост.
+		 * Обновляет состояние лайков на клиенте и синхронизирует его с сервером.
 		 */
 		toggleLike: (postId, userId) => {
 			set((state) => {
+				// Обновляем локально состояние лайков в постах
 				const updatedPosts = state.posts.map((post) => {
 					if (post._id === postId) {
+						// Проверяем, лайкал ли пользователь этот пост ранее
 						const isLiked = post.likes.includes(userId);
 						const updatedLikes = isLiked
-							? post.likes.filter((id) => id !== userId)
-							: [...post.likes, userId];
+							? post.likes.filter((id) => id !== userId) // Удаляем лайк, если он уже есть
+							: [...post.likes, userId]; // Добавляем лайк, если его не было
 
-						return { ...post, likes: updatedLikes };
+						return { ...post, likes: updatedLikes }; // Возвращаем обновленный пост с обновленным списком лайков.
 					}
-					return post;
+					return post; // Возвращаем пост без изменений, если ID не совпадает.
 				});
 
-				// Сохраняем обновленные посты в localStorage
+				// Сохраняем обновленные посты в localStorage для сохранения состояния после перезагрузки страницы.
 				localStorage.setItem('posts', JSON.stringify(updatedPosts));
 
-				// Возвращаем новое состояние
+				// Возвращаем новое состояние, обновляя posts в Zustand store.
 				return { posts: updatedPosts };
 			});
 
-			// Синхронизируем с сервером
+			// Синхронизируем изменения с сервером, вызвав likePost.
 			get().likePost(postId, userId);
 		},
 	}))
