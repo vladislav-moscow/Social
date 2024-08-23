f1db5dfdf8452fc11836d3b6ace0d38ac627a91e
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-23 21:16:15 +0300

    отображение и переход на страницу пользователя

--- client/src/components/Friend/Friend.jsx
+++ client/src/components/Friend/Friend.jsx
@@ -1,14 +1,41 @@
-import "./friend.css";
+import { useEffect } from 'react';
+import './friend.css';
+import useUserStore from '../../store/useUserStore';
+import { Link } from 'react-router-dom';
 
-const Friend = ({ user }) => {
+const Friend = ({ friendId }) => {
 	const PF = import.meta.env.VITE_PUBLIC_FOLDER;
+	const fetchUser = useUserStore((state) => state.fetchUser);
+	const getUserById = useUserStore((state) => state.getUserById);
+
+	// Получаем данные друга из стора
+	const friend = getUserById(friendId);
+
+	useEffect(() => {
+		if (!friend) {
+			// Загружаем данные друга, если они еще не загружены
+			fetchUser(friendId);
+		}
+	}, [friendId, friend, fetchUser]);
+
+	if (!friend) return null; // Возвращаем null, если друг еще не загружен
 
 	return (
-		<li className="sidebarFriend">
-      <img className="sidebarFriendImg" src={PF+user.profilePicture} alt="" />
-      <span className="sidebarFriendName">{user.username}</span>
-    </li>
-	)
+		<li className='sidebarFriend'>
+			<Link to={`/profile/${friend.username}`}>
+				<img
+					className='sidebarFriendImg'
+					src={
+						friend.profilePicture
+							? PF + friend.profilePicture
+							: PF + 'person/noAvatar.png'
+					}
+					alt={friend.username}
+				/>
+				<span className='sidebarFriendName'>{friend.username}</span>
+			</Link>
+		</li>
+	);
 };
 
 export default Friend;
