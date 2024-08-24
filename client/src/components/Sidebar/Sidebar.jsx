bbf15206735531b4e9b0731584e2cb63dbb6d829
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-24 21:38:42 +0300

    поправил стили и прокоментировал код

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -13,25 +13,40 @@ import './sidebar.css';
 import { useEffect } from 'react';
 import useUserStore from '../../store/useUserStore';
 
+/**
+ * Компонент для отображения боковой панели с навигацией и списком друзей.
+ * @returns {JSX.Element} Компонент боковой панели.
+ */
 const Sidebar = () => {
-	// Получаем текущего пользователя из Zustand Store
+	// Получаем текущего пользователя из Store.
 	const user = useAuthStore((state) => state.user);
+	// Функция для загрузки данных пользователя по ID.
 	const fetchUser = useUserStore((state) => state.fetchUser);
+	// Функция для получения данных пользователя по его ID.
 	const getUserById = useUserStore((state) => state.getUserById);
+	// Флаг загрузки данных.
 	const isFetching = useUserStore((state) => state.isFetching);
+	// Сообщение об ошибке, если таковая имеется.
 	const error = useUserStore((state) => state.error);
 
+	/**
+	 * Эффект для загрузки данных друзей пользователя при изменении `user` или его `followings`.
+	 * Запрашивает данные для каждого друга, если они еще не загружены.
+	 */
 	useEffect(() => {
 		if (user && user.followings) {
+			// Для каждого ID друга проверяем, загружены ли данные в состояние.
 			user.followings.forEach((friendId) => {
 				if (!getUserById(friendId)) {
-					fetchUser(friendId);
+					fetchUser(friendId); // Загружаем данные пользователя по ID.
 				}
 			});
 		}
 	}, [user, fetchUser, getUserById]);
 
+	// Отображаем индикатор загрузки, если данные еще загружаются.
 	if (isFetching) return <p>Загрузка...</p>;
+	// Отображаем сообщение об ошибке, если произошла ошибка при загрузке данных.
 	if (error) return <p>Ошибка: {error}</p>;
 
 	return (
