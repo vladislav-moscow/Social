2663afd4dbfe883d02c44334ccbe147ccec4a852
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-24 10:42:12 +0300

    вывожу друзей из стора

--- client/src/components/Friend/Friend.jsx
+++ client/src/components/Friend/Friend.jsx
@@ -3,20 +3,25 @@ import './friend.css';
 import useUserStore from '../../store/useUserStore';
 import { Link } from 'react-router-dom';
 
+/**
+ * Компонент для отображения информации о друге.
+ * @param {string} friendId - ID друга, чьи данные нужно загрузить и отобразить.
+ */
 const Friend = ({ friendId }) => {
-	const PF = import.meta.env.VITE_PUBLIC_FOLDER;
-	const fetchUser = useUserStore((state) => state.fetchUser);
-	const getUserById = useUserStore((state) => state.getUserById);
+	const PF = import.meta.env.VITE_PUBLIC_FOLDER; // Путь к публичной папке для загрузки изображений.
+	const fetchUser = useUserStore((state) => state.fetchUser); // Функция для загрузки данных пользователя.
+	const getUserById = useUserStore((state) => state.getUserById); // Функция для получения данных пользователя по его ID.
 
 	// Получаем данные друга из стора
 	const friend = getUserById(friendId);
 
+	// Используем useEffect для загрузки данных друга, если они еще не загружены.
 	useEffect(() => {
 		if (!friend) {
-			// Загружаем данные друга, если они еще не загружены
+			// Загружаем данные друга, если они не найдены в состоянии.
 			fetchUser(friendId);
 		}
-	}, [friendId, friend, fetchUser]);
+	}, [friendId, friend, fetchUser]); // Зависимости включают friendId, friend и fetchUser.
 
 	if (!friend) return null; // Возвращаем null, если друг еще не загружен
 
