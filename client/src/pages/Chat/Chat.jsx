41fe489e890758ca1d88b2d5f52bbe7aecc6ddfa
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-28 22:28:52 +0300

    получаю данные из стора беседы и сообщения и создаю новое сообщение

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,14 +1,42 @@
 import './chat.css';
-//import Sidebar from "../../components/Sidebar/Sidebar";
 import Topbar from '../../components/Topbar/Topbar';
 import SidebarList from '../../components/SidebarList/SidebarList';
 import Conversation from '../../components/Conversations/Conversation';
 import useAuthStore from '../../store/useAuthStore';
 import Message from '../../components/Message/Message';
 import ChatOnline from '../../components/ChatOnline/ChatOnline';
+import { useEffect, useState } from 'react';
+import useConversationStore from '../../store/useConversationStore';
+import useMessageStore from '../../store/useMessageStore';
 
 const Chat = () => {
+	const { conversations, fetchConversations } = useConversationStore();
 	const user = useAuthStore((state) => state.getUser());
+	const { messages, fetchMessages, sendMessage } = useMessageStore();
+	const [currentChat, setCurrentChat] = useState(null);
+	const [newMessage, setNewMessage] = useState('');
+
+	useEffect(() => {
+		fetchConversations(user._id);
+	}, [user._id, fetchConversations]);
+
+	useEffect(() => {
+		if (currentChat) {
+			fetchMessages(currentChat._id);
+		}
+	}, [currentChat, fetchMessages]);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const message = {
+			sender: user._id,
+			text: newMessage,
+			conversationId: currentChat._id,
+		};
+		await sendMessage(message);
+		setNewMessage('');
+	};
+
 	return (
 		<>
 			<Topbar />
@@ -20,28 +48,41 @@ const Chat = () => {
 						</div>
 
 						<input placeholder='Search for friends' className='chatMenuInput' />
-						{
-							<div>
-								<Conversation user={user} />
+						{conversations.map((c) => (
+							<div key={c._id} onClick={() => setCurrentChat(c)}>
+								<Conversation conversation={c} currentUser={user} />
 							</div>
-						}
+						))}
 					</div>
 				</div>
 				<div className='chatBox'>
 					<div className='chatBoxWrapper'>
-						<div className='chatBoxTop'>
-							<Message />
-							<Message own={true} />
-							<Message />
-							<Message own={true} />
-						</div>
-						<div className='chatBoxBottom'>
-							<textarea
-								className='chatMessageInput'
-								placeholder='напишите сообщение...'
-							></textarea>
-							<button className='chatSubmitButton'>Отправить</button>
-						</div>
+						{currentChat ? (
+							<>
+								<div className='chatBoxTop'>
+									{messages[currentChat._id]?.map((m) => (
+										<div key={m._id}>
+											<Message message={m} own={m.sender === user._id} />
+										</div>
+									))}
+								</div>
+								<div className='chatBoxBottom'>
+									<textarea
+										className='chatMessageInput'
+										placeholder='напишите сообщение...'
+										onChange={(e) => setNewMessage(e.target.value)}
+										value={newMessage}
+									></textarea>
+									<button className='chatSubmitButton' onClick={handleSubmit}>
+										Отправить
+									</button>
+								</div>
+							</>
+						) : (
+							<span className='noConversationText'>
+								Откройте бесседу и начните чат!
+							</span>
+						)}
 					</div>
 				</div>
 				<div className='chatOnline'>
