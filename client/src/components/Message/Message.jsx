c981355131a8f494bd88ccbea6cb83590bef407d
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-08-29 22:03:32 +0300

    добавил отображение картинки и стилизовал

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,29 +1,38 @@
 import { format, register } from 'timeago.js';
 import ru from 'timeago.js/lib/lang/ru';
-import "./message.css";
+import './message.css';
 
 // Регистрируем русскую локализацию для timeago.js, чтобы отображать дату и время в русском формате.
 register('ru', ru);
 
-const Message = ({ message, own}) => {
+const Message = ({ message, own }) => {
 	const PF = import.meta.env.VITE_PUBLIC_FOLDER;
 
 	// Форматируем дату создания поста с использованием timeago.js и русской локализации.
 	const formattedDate = format(message.createdAt, 'ru');
 
 	return (
-		<div className={own? "message own" : "message"}>
-      <div className="messageTop">
-        <img
-          className="messageImg"
-          src={PF + 'person/1.jpg'}
-          alt=""
-        />
-        <p className="messageText">{message.text}</p>
-      </div>
-      <div className="messageBottom">{formattedDate}</div>
-    </div>
-  );
-}
+		<div className={own ? 'message own' : 'message'}>
+			<div className='messageTop'>
+				<img className='messageImg' src={PF + 'person/1.jpg'} alt='' />
+				<div className='messageContent'>
+					{message.img ? (
+						<div className='messageText textImg'>
+							<img
+									className='messageImgContent'
+									src={PF + message.img} // Путь к изображению
+									alt='Message attachment'
+								/>
+								{message.text && <span className='messageTextInImg'>{message.text}</span>}
+						</div>
+					) : (
+						<div className='messageText'>{message.text}</div>
+					)}
+				</div>
+			</div>
+			<div className='messageBottom'>{formattedDate}</div>
+		</div>
+	);
+};
 
 export default Message;
