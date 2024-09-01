6c67600694319c427123b0ff6b343fff3ce444d0
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-01 21:18:45 +0300

    настроил ссылки на курсы и работы

--- client/src/components/SidebarList/SidebarList.jsx
+++ client/src/components/SidebarList/SidebarList.jsx
@@ -1,4 +1,4 @@
-import "./sidebarList.css";
+import './sidebarList.css';
 import {
 	Chat,
 	Group,
@@ -13,41 +13,45 @@ const SidebarList = () => {
 	return (
 		<>
 			<ul className='sidebarList'>
-					<li className='sidebarListItem'>
-						<Link to={'/'}>
-							<RssFeed htmlColor='#1877f2' className='sidebarIcon' />
-							<span className='sidebarListItemText'>Новости</span>
-						</Link>
-					</li>
-					<li className='sidebarListItem'>
-						<Link to={'/chat'}>
-							<Chat htmlColor='#1877f2' className='sidebarIcon' />
-							<span className='sidebarListItemText'>Чат</span>
-						</Link>
-					</li>
-					<li className='sidebarListItem'>
-						<PlayCircleFilledOutlined
-							htmlColor='#1877f2'
-							className='sidebarIcon'
-						/>
-						<span className='sidebarListItemText'>Видео</span>
-					</li>
-					<li className='sidebarListItem'>
-						<Group htmlColor='#1877f2' className='sidebarIcon' />
-						<span className='sidebarListItemText'>Группы</span>
-					</li>
-					<li className='sidebarListItem'>
+				<li className='sidebarListItem'>
+					<Link to={'/'}>
+						<RssFeed htmlColor='#1877f2' className='sidebarIcon' />
+						<span className='sidebarListItemText'>Новости</span>
+					</Link>
+				</li>
+				<li className='sidebarListItem'>
+					<Link to={'/chat'}>
+						<Chat htmlColor='#1877f2' className='sidebarIcon' />
+						<span className='sidebarListItemText'>Чат</span>
+					</Link>
+				</li>
+				<li className='sidebarListItem'>
+					<PlayCircleFilledOutlined
+						htmlColor='#1877f2'
+						className='sidebarIcon'
+					/>
+					<span className='sidebarListItemText'>Видео</span>
+				</li>
+				<li className='sidebarListItem'>
+					<Group htmlColor='#1877f2' className='sidebarIcon' />
+					<span className='sidebarListItemText'>Группы</span>
+				</li>
+				<li className='sidebarListItem'>
+					<Link to={'/job'}>
 						<WorkOutline htmlColor='#1877f2' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Работа</span>
-					</li>
-					<li className='sidebarListItem'>
+					</Link>
+				</li>
+				<li className='sidebarListItem'>
+					<Link to={'/course'}>
 						<School htmlColor='#1877f2' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Курсы</span>
-					</li>
-				</ul>
-				<hr className='sidebarHr' />
+					</Link>
+				</li>
+			</ul>
+			<hr className='sidebarHr' />
 		</>
 	);
-}
+};
 
 export default SidebarList;
