765af9cb350a9761587f1e86875956f729a953c5
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-01 12:30:27 +0300

    сделал друзья онлайн и размести их на странице чата

--- client/src/components/ChatOnline/ChatOnline.jsx
+++ client/src/components/ChatOnline/ChatOnline.jsx
@@ -1,17 +1,59 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import './chatOnline.css';
 
-const ChatOnline = () => {
+const ChatOnline = ({ onlineUsers, currentId, setCurrentChat }) => {
+	const [friends, setFriends] = useState([]);
+	const [onlineFriends, setOnlineFriends] = useState([]);
 	const PF = import.meta.env.VITE_PUBLIC_FOLDER;
+
+	useEffect(() => {
+		const getFriends = async () => {
+			const res = await axios.get('/api/users/friends/' + currentId);
+			setFriends(res.data);
+		};
+
+		getFriends();
+	}, [currentId]);
+
+	useEffect(() => {
+		setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+	}, [friends, onlineUsers]);
+
+	const handleClick = async (user) => {
+		try {
+			const res = await axios.get(
+				`/api/conversations/find/${currentId}/${user._id}`
+			);
+			setCurrentChat(res.data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	console.log(onlineFriends);
 
 	return (
 		<div className='chatOnline'>
-			<div className='chatOnlineFriend'>
-				<div className='chatOnlineImgContainer'>
-					<img className='chatOnlineImg' src={PF + 'person/10.jpg'} alt='' />
-					<div className='chatOnlineBadge'></div>
+			{onlineFriends.map((o) => (
+				
+				<div key={o._id} className='chatOnlineFriend' onClick={() => handleClick(o)}>
+					<div className='chatOnlineImgContainer'>
+						<img
+							className='chatOnlineImg'
+							src={
+								o?.profilePicture
+									? PF + o.profilePicture
+									: PF + 'person/noAvatar.png'
+							}
+							alt=''
+						/>
+						<div className='chatOnlineBadge'></div>
+					</div>
+					<span className='chatOnlineName'>{o?.username}</span>
 				</div>
-				<span className='chatOnlineName'>John Snow</span>
-			</div>
+			))}
 		</div>
 	);
 };
