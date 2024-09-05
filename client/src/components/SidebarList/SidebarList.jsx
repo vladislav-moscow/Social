ac3e7e7ff1b68c78dba0acf7215eb248dceb6147
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-05 20:24:58 +0300

    поменял цвета в приложении

--- client/src/components/SidebarList/SidebarList.jsx
+++ client/src/components/SidebarList/SidebarList.jsx
@@ -15,36 +15,36 @@ const SidebarList = () => {
 			<ul className='sidebarList'>
 				<li className='sidebarListItem'>
 					<Link to={'/'}>
-						<RssFeed htmlColor='#1877f2' className='sidebarIcon' />
+						<RssFeed htmlColor='#8a2667' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Новости</span>
 					</Link>
 				</li>
 				<li className='sidebarListItem'>
 					<Link to={'/chat'}>
-						<Chat htmlColor='#1877f2' className='sidebarIcon' />
+						<Chat htmlColor='#8a2667' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Чат</span>
 					</Link>
 				</li>
 				<li className='sidebarListItem'>
 					<PlayCircleFilledOutlined
-						htmlColor='#1877f2'
+						htmlColor='#8a2667'
 						className='sidebarIcon'
 					/>
 					<span className='sidebarListItemText'>Видео</span>
 				</li>
 				<li className='sidebarListItem'>
-					<Group htmlColor='#1877f2' className='sidebarIcon' />
+					<Group htmlColor='#8a2667' className='sidebarIcon' />
 					<span className='sidebarListItemText'>Группы</span>
 				</li>
 				<li className='sidebarListItem'>
 					<Link to={'/job'}>
-						<WorkOutline htmlColor='#1877f2' className='sidebarIcon' />
+						<WorkOutline htmlColor='#8a2667' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Работа</span>
 					</Link>
 				</li>
 				<li className='sidebarListItem'>
 					<Link to={'/course'}>
-						<School htmlColor='#1877f2' className='sidebarIcon' />
+						<School htmlColor='#8a2667' className='sidebarIcon' />
 						<span className='sidebarListItemText'>Курсы</span>
 					</Link>
 				</li>
