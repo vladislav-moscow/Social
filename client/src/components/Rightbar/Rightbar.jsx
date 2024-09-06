e592f27679ea791de930e90d8eb56d5a5b8a536f
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-06 23:46:37 +0300

    из пользователе онлайн убераю текущего пользователя получаю данные их и отображаю в колонке онлайн

--- client/src/components/Rightbar/Rightbar.jsx
+++ client/src/components/Rightbar/Rightbar.jsx
@@ -1,16 +1,37 @@
+import { useEffect, useState } from 'react';
+import useAuthStore from '../../store/useAuthStore';
 import './rightbar.css';
+import useUserStore from '../../store/useUserStore';
+import Online from '../Online/Online';
 
-const Rightbar = ({ user }) => {
+const Rightbar = ({ user, currentUser }) => {
+	const onlineUsers = useAuthStore((state) => state.onlineUsers);
+	const [onlineFriends, setOnlineFriends] = useState([]);
+	const { users } = useUserStore((state) => ({
+		users: state.users,
+	}));
+
+	useEffect(() => {
+		if (currentUser) {
+			// Фильтрация списка друзей, исключая текущего пользователя
+			const friendsArray = Object.values(users).filter(
+				(user) => user._id !== currentUser._id && onlineUsers.includes(user._id)
+			);
+
+			setOnlineFriends(friendsArray);
+		}
+	}, [users, onlineUsers, currentUser, user]);
 
 	const HomeRightbar = () => {
-		
-	
-		
 		return (
 			<>
 				<h4 className='rightbarTitle'>В сети:</h4>
 				<ul className='rightbarFriendList'>
-				
+					{onlineFriends.map((user) => (
+						<div key={user._id}>
+							<Online user={user} />
+						</div>
+					))}
 				</ul>
 			</>
 		);
@@ -23,11 +44,15 @@ const Rightbar = ({ user }) => {
 				<div className='rightbarInfo'>
 					<div className='rightbarInfoItem'>
 						<span className='rightbarInfoKey'>Город:</span>
-						<span className='rightbarInfoValue'>{user.city ? user.city : 'нет данных'}</span>
+						<span className='rightbarInfoValue'>
+							{user.city ? user.city : 'нет данных'}
+						</span>
 					</div>
 					<div className='rightbarInfoItem'>
 						<span className='rightbarInfoKey'>Родной город:</span>
-						<span className='rightbarInfoValue'>{user.from ? user.from : 'нет данных'}</span>
+						<span className='rightbarInfoValue'>
+							{user.from ? user.from : 'нет данных'}
+						</span>
 					</div>
 					<div className='rightbarInfoItem'>
 						<span className='rightbarInfoKey'>Семейное положение:</span>
