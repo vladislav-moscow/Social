e18cc1be2fff561bb16bbdb7e1778b57dbdcda88
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-07 21:56:17 +0300

    добавил компонент рекламы

---
+++ client/src/components/Advertisement/Advertisement.jsx
@@ -0,0 +1,46 @@
+import './advertisement.css';
+
+const Advertisement = () => {
+	return (
+		<div className='advertisementBlock'>
+			<div className='advertisementWrapper'>
+				<div className='advertisementImage'></div>
+				<div className='advertisementTitle'>
+					Квартиры премиум-класса в небоскребе Famous от 25,7 млн
+				</div>
+				<p className='advertisementDesc'>
+					Монолит готов. Рядом с Кутузовским проспектом. Варианты отделки на
+					выбор
+				</p>
+				<div className='advertisementLinks'>
+					<a
+						href='https://www.mr-group.ru/projects/famous/?utm_source=yandex&utm_medium=cpc&utm_campaign=ya_filicity_msk_n_general&utm_term=%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F&utm_content=dop_1||cid_96180774||grid_5287063730||bnid_15022283347||phid_47167836065||key_%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F||ps_0||pst_none||src_dzen.ru||srct_context||addph_no||re_47167836065||dev_desktop||reg_213&cm_id=96180774_5287063730_15022283347_47167836065_47167836065_dzen.ru_context_type1_no_desktop_none_213&yclid=1297058118961463295'
+						className='advertsementLink'
+					>
+						Умный дом
+					</a>
+					<a
+						href='https://www.mr-group.ru/projects/famous/?utm_source=yandex&utm_medium=cpc&utm_campaign=ya_filicity_msk_n_general&utm_term=%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F&utm_content=dop_2||cid_96180774||grid_5287063730||bnid_15022283347||phid_47167836065||key_%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F||ps_0||pst_none||src_dzen.ru||srct_context||addph_no||re_47167836065||dev_desktop||reg_213&cm_id=96180774_5287063730_15022283347_47167836065_47167836065_dzen.ru_context_type1_no_desktop_none_213&yclid=1297058118961463295'
+						className='advertsementLink'
+					>
+						Trad In
+					</a>
+				</div>
+				<a
+					href='https://www.mr-group.ru/projects/famous/?utm_source=yandex&utm_medium=cpc&utm_campaign=ya_filicity_msk_n_general&utm_term=%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F&utm_content=render_famous_march||cid_96180774||grid_5287063730||bnid_15022283347||phid_47167836065||key_%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F||ps_0||pst_none||src_dzen.ru||srct_context||addph_no||re_47167836065||dev_desktop||reg_213&cm_id=96180774_5287063730_15022283347_47167836065_47167836065_dzen.ru_context_type1_no_desktop_none_213&yclid=1297058118961463295'
+					className='advertisementSite'
+				>
+					mr-group.ru
+				</a>
+				<a
+					href='https://www.mr-group.ru/projects/famous/?utm_source=yandex&utm_medium=cpc&utm_campaign=ya_filicity_msk_n_general&utm_term=%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F&utm_content=render_famous_march||cid_96180774||grid_5287063730||bnid_15022283347||phid_47167836065||key_%D0%BA%D1%83%D0%BF%D0%B8%D1%82%D1%8C%20%D1%82%D1%80%D0%B5%D1%85%20%D0%BA%D0%BE%D0%BC%D0%BD%D0%B0%D1%82%D0%BD%D0%B0%D1%8F||ps_0||pst_none||src_dzen.ru||srct_context||addph_no||re_47167836065||dev_desktop||reg_213&cm_id=96180774_5287063730_15022283347_47167836065_47167836065_dzen.ru_context_type1_no_desktop_none_213&yclid=1297058118961463295'
+					className='advertisementBtn'
+				>
+					Перейти на сайт
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Advertisement;
