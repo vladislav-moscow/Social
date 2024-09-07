d570174ae00bbe0d6d63dfa502b4a94e41059c01
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-07 22:06:29 +0300

    удалил ненужные строки кода потому что получаю данные уже готовые

--- client/src/components/Friend/Friend.jsx
+++ client/src/components/Friend/Friend.jsx
@@ -1,33 +1,20 @@
-import { useEffect } from 'react';
 import './friend.css';
-import useUserStore from '../../store/useUserStore';
 import { Link } from 'react-router-dom';
 
 /**
- * Компонент для отображения информации о друге.
+ * Компонент `Friend` отображает информацию о друге и предоставляет ссылку на его профиль.
+ *
  * @param {Object} props - Свойства компонента.
- * @param {string} props.friendId - ID друга, чьи данные нужно загрузить и отобразить.
- * @returns {JSX.Element|null} - Возвращает JSX элемент с информацией о друге или null, если данные не загружены.
+ * @param {Object} props.friend - Объект, содержащий информацию о друге.
+ * @param {string} props.friend.username - Имя пользователя друга.
+ * @param {string} props.friend.profilePicture - URL изображения профиля друга.
+ *
+ * @returns {JSX.Element|null} - Возвращает JSX элемент с информацией о друге или null, если данные не переданы.
  */
-const Friend = ({ friendId }) => {
-	const PF = import.meta.env.VITE_PUBLIC_FOLDER; // Путь к публичной папке для загрузки изображений.
-
-	// Получаем функции из стора пользователей для загрузки и получения данных пользователя.
-	const fetchUser = useUserStore((state) => state.fetchUser); // Функция для загрузки данных пользователя по его ID.
-	const getUserById = useUserStore((state) => state.getUserById); // Функция для получения данных пользователя из состояния стора.
-
-	// Получаем данные друга из стора по переданному ID.
-	const friend = getUserById(friendId);
-
-	// Используем useEffect для загрузки данных друга, если они еще не загружены.
-	useEffect(() => {
-		if (!friend) {
-			// Если данные друга еще не загружены, вызываем fetchUser для их получения.
-			fetchUser(friendId);
-		}
-	}, [friendId, friend, fetchUser]); // Зависимости useEffect включают friendId, состояние друга и функцию fetchUser.
-
-	// Если данные друга еще не загружены, возвращаем null (не отображаем компонент).
+const Friend = ({ friend }) => {
+	// Путь к публичной папке для загрузки изображений.
+	const PF = import.meta.env.VITE_PUBLIC_FOLDER; 
+	// Если данные друга не переданы, возвращаем null (не отображаем компонент).
 	if (!friend) return null;
 
 	return (
