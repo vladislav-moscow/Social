4028b02d6f421849bf8e1acabfb194acc74541f9
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-09-07 21:59:14 +0300

    прокоментировал компоненты

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -6,22 +6,29 @@ import './message.css';
 // Регистрируем русскую локализацию для timeago.js, чтобы отображать дату и время в русском формате.
 register('ru', ru);
 
+/**
+ * Компонент `Message` отображает сообщение в чате.
+ *
+ * @param {Object} props - Свойства компонента.
+ * @param {Object} props.message - Объект сообщения, который нужно отобразить.
+ * @param {boolean} props.own - Флаг, указывающий, принадлежит ли сообщение текущему пользователю.
+ * @param {Object} props.currentUser - Данные текущего пользователя.
+ *
+ * @returns {JSX.Element} Компонент, отображающий сообщение с возможными вложениями.
+ */
+
 const Message = ({ message, own, currentUser}) => {
-	const PF = import.meta.env.VITE_PUBLIC_FOLDER;
+	// Путь к публичной папке для загрузки изображений
+	const PF = import.meta.env.VITE_PUBLIC_FOLDER; 
 	// Используем Zustand store для получения и загрузки данных пользователя
 	const { getUserById } = useUserStore((state) => ({
 		getUserById: state.getUserById,
 	}));
-
-	// Получаем ID отправителя
-	const senderId = message.sender; 
-	//console.log(senderId);// Предполагаем, что senderId хранится в сообщении
-
+	// Получаем ID отправителя сообщения
+	const senderId = message.sender;
 	// Получаем данные пользователя по ID отправителя
 	const sender = getUserById(senderId);
-	//console.log(sender)
-
-	// Форматируем дату создания поста с использованием timeago.js и русской локализации.
+	// Форматируем дату создания сообщения с использованием timeago.js и русской локализации
 	const formattedDate = format(message.createdAt, 'ru');
 
 	return (
@@ -30,11 +37,11 @@ const Message = ({ message, own, currentUser}) => {
 				{own ? <img
 					className='messageImg'
 					src={currentUser?.profilePicture ? PF + currentUser?.profilePicture : PF + 'person/noAvatar.png'}
-					alt=''
+					alt={currentUser.username}
 				/>:<img
 					className='messageImg'
 					src={sender?.profilePicture ? PF + sender?.profilePicture : PF + 'person/noAvatar.png'}
-					alt=''
+					alt={sender.username}
 				/>}
 				
 				<div className='messageContent'>
@@ -42,7 +49,7 @@ const Message = ({ message, own, currentUser}) => {
 						<div className='messageText textImg'>
 							<img
 								className='messageImgContent'
-								src={PF + message.img} // Путь к изображению
+								src={PF + message.img}
 								alt='Message attachment'
 							/>
 							{message.text && (
